5b866ff48159104af97f526e57c4c44e7ee9156c
Dan Fu <danfu@dans-mbp.lan>
2023-11-20 17:03:59 -0500

    Added a new Activity called UserComplaintActivityScreen, doesn't have anything just a basic empty starting point.

---
+++ app/src/main/java/com/example/cscb07_project/UserComplaintActivityScreen.java
@@ -0,0 +1,13 @@
+package com.example.cscb07_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserComplaintActivityScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_complaint_screen);
+    }
+}
