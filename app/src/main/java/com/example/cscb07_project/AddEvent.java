5b2c41d480199efb44fd963ecff572dca66b2b66
cailene <144947050+cailene@users.noreply.github.com>
2023-11-23 19:35:56 -0500

    Added comments for all files. Added more properties for events and modified layouts to accommodate for new properites. Also added the activities to AndroidManifest.xml

--- app/src/main/java/com/example/cscb07_project/AddEvent.java
+++ app/src/main/java/com/example/cscb07_project/AddEvent.java
@@ -2,13 +2,94 @@ package com.example.cscb07_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * AddEvent Class allows users to make and add a new event
+ */
 public class AddEvent extends AppCompatActivity {
+    EditText eventDetails, eventName;
+    DatabaseReference db;
+    Button addEventButton;
 
+    /**
+     * Setting up UI
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_event);
+        setContentView(R.layout.activity_add_new_event);
+
+        // UI components
+        // connecting ids to input fields for user to add stuff
+        eventName = findViewById(R.id.eventDetailName);
+        eventDetails = findViewById(R.id.eventDetails);
+        // button that users click to add event
+        addEventButton = findViewById(R.id.addEventButton);
+
+        // getting "Events" reference from the Firebase Database
+        db = FirebaseDatabase.getInstance().getReference("Events");
+        // Log.d is just for debugging purposes
+        Log.d("DatabaseReference", "Database reference: " + db.toString());
+        // adding a listener on "Add" event button
+        addEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // getting text from user
+                String name = eventName.getText().toString();
+                String details = eventDetails.getText().toString();
+
+                // shows a toast that asks user to write data if the event fields are empty
+                // o/w adding data to database then creates an intent to start the EventList activity
+                if (TextUtils.isEmpty(name) && TextUtils.isEmpty(details)) {
+                    Toast.makeText(AddEvent.this, "Please add some data.", Toast.LENGTH_SHORT).show();
+                } else {
+                    addDataToFirebase(name, details);
+                    Intent intent = new Intent(AddEvent.this, EventList.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    /** addDataToFirebase adds data to db
+     *
+     * @param name
+     * @param details
+     */
+    private void addDataToFirebase(String name, String details) {
+        Log.d("AddData", "Adding data to Firebase: " + name + ", " + details);
+        // generate a unique key which will be the event ID
+        DatabaseReference eventRef = db.push();
+        Log.d("AddData", "Event ID" + eventRef);
+
+        // checks if the key was generated properly
+        // then makes a new Event object, sets it's values, adds it to the db
+        // displays a toast on whether data was added successfully or not
+        if (eventRef != null) {
+            Event newEvent = new Event(name, details, false);
+            eventRef.setValue(newEvent)
+                    .addOnSuccessListener(aVoid -> Log.d("AddData", "Data added successfully"))
+                    .addOnFailureListener(e -> Log.e("AddData", "Error adding data to Firebase", e));
+
+            Toast.makeText(AddEvent.this, "Data added", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(AddEvent.this, "Failed to generate a unique key", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/cscb07_project/EventDetails.java
+++ app/src/main/java/com/example/cscb07_project/EventDetails.java
@@ -1,14 +1,107 @@
 package com.example.cscb07_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+/**
+ * display the details of a specific event, retrieved from the Firebase Realtime Database,
+ * and allows the user to update the RSVP status of that event
+ */
 public class EventDetails extends AppCompatActivity {
+    // represents the TextView elements in the event details layout
+    TextView retrieveEventName, retrieveEventDetails, retrieveEventId;
+    // represents the button that allows the user to update the RSVP status
+    Button retrieveEventRSVP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Sets the content view of the activity
         setContentView(R.layout.activity_event_details);
+
+        Log.d("EventDetails", "onCreate: Activity started");
+
+        // Initializes the UI elements
+        retrieveEventName = findViewById(R.id.eventDetailName);
+        retrieveEventDetails = findViewById(R.id.eventDetails);
+        retrieveEventRSVP = findViewById(R.id.rsvpButton);
+
+        // Retrieves the event ID passed from the previous activity (EventList) through the intent
+        String eventId = getIntent().getStringExtra("Event ID");
+        //retrieveEventId.setText(eventId);
+
+        retrieveEventData(eventId);
+
+        // Sets a click listener on the RSVP button to update the RSVP status when clicked
+        retrieveEventRSVP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateDatabase(eventId);
+            }
+        });
+
+        // Logs messages for debugging purposes
+        Log.d("EventDetails", "onCreate: End of activity");
+    }
+
+    /** Retrieves event data from the Firebase Realtime Database based on the provided event ID
+     *
+     * @param eventId is the event data from the Firebase Realtime Database
+     */
+    private void retrieveEventData(String eventId) {
+        // gets reference to "eventId" child key from "Events" key
+        DatabaseReference eventRef = FirebaseDatabase.getInstance().getReference("Events").child(eventId);
+
+        // listeners to changes to db and displays the UI based on db
+        eventRef.addValueEventListener(new ValueEventListener() {
+            //onDataChange method, checks if the snapshot of the data exists,
+            // then retrieves the Event object and updates the corresponding TextView
+            // and the RSVP button with the event details
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    Event event = snapshot.getValue(Event.class);
+                    if (event != null) {
+                        retrieveEventName.setText(event.getEventName());
+                        retrieveEventDetails.setText(event.getEventDetails());
+                        retrieveEventRSVP.setText(event.getRsvpBool() ? "Confirmed!" : "RSVP");
+                    }
+                }
+            }
+
+            // onCancelled method, logs an error message if there's an issue
+            // reading data from Firebase
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("EventDetails", "Error reading data from Firebase", error.toException());
+            }
+        });
+    }
+
+    /** updates the RSVP status of the event in the Firebase Realtime Database
+     *
+     * @param eventId is the id of the event
+     */
+    private void updateDatabase(String eventId) {
+        // gets reference to "eventId" child key from "Events" key
+        DatabaseReference eventRef = FirebaseDatabase.getInstance().getReference("Events").child(eventId);
+
+        // modifies the rsvpBool field based on the current text of the RSVP button
+        eventRef.child("rsvpBool").setValue(!retrieveEventRSVP.getText().equals("Confirmed!"))
+                .addOnSuccessListener(aVoid -> Log.d("EventDetails", "RSVP status updated successfully"))
+                .addOnFailureListener(e -> Log.e("EventDetails", "Error updating RSVP status", e));
     }
 }
