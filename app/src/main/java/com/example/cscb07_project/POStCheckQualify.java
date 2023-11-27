e92f189c510100713accd3e15ce2c9a8cb3b9eed
apilina <shroomshi@gmail.com>
2023-11-27 01:53:23 -0500

    - updated post temp file - added helper methods calc1Group, calc2Group, linGroup for POStCheckQualify class

--- app/src/main/java/com/example/cscb07_project/POStCheckQualify.java
+++ app/src/main/java/com/example/cscb07_project/POStCheckQualify.java
@@ -7,10 +7,32 @@ public abstract class POStCheckQualify {
 
     ArrayList<String> courses;
 
+    public POStCheckQualify() {
+        courses = new ArrayList<String>();
+    }
+
     public boolean creditRequirement(POStBasicInfo info) {
         return (info.getAnswer("Select how many credits you have completed currently:")).equals("4.0 or more");
     }
 
+    public boolean calc1Group(POStGPAInfo info) {
+        // System.out.println(info.getAnswer("MATA30"));
+        // System.out.println(info.getAnswer("MATA31"));
+        return (info.courseExists("MATA30")) || (info.courseExists("MATA31"));
+    }
+
+    public boolean calc2Group(POStGPAInfo info) {
+        // System.out.println(info.getAnswer("MATA37"));
+        // System.out.println(info.getAnswer("MATA36"));
+        return (info.courseExists("MATA37")) || (info.courseExists("MATA36"));
+    }
+
+    public boolean linGroup(POStGPAInfo info) {
+        // System.out.println(info.getAnswer("MATA22"));
+        // System.out.println(info.getAnswer("MATA23"));
+        return (info.courseExists("MATA22")) || (info.courseExists("MATA23"));
+    }
+
     abstract public boolean hasAllCourses(POStGPAInfo info);
-    abstract public boolean meetsGPARequirements(POStGPAInfo marks);
+    abstract public boolean meetsGPARequirements(POStGPAInfo info);
 }
--- app/src/main/java/com/example/cscb07_project/POStCompSciMinor.java
+++ app/src/main/java/com/example/cscb07_project/POStCompSciMinor.java
@@ -5,20 +5,33 @@ import java.util.ArrayList;
 
 public class POStCompSciMinor extends POStCheckQualify {
 
-    @Override
-    public boolean hasAllCourses(POStGPAInfo info) {
-        for(int i = 0; i < courses.size(); i++) {
-            String course = courses.get(i);
-            if(info.getAnswer(course) == (-1)) {
-                return false;
-            }
-        }
-
-        return true;
+    public POStCompSciMinor() {
+        super();
+        courses.add("CSCA08");
+        courses.add("CSCA48");
+        // courses.add("CSCA67");
+        // courses.add("MATA67");
+        // courses.add("MATA22");
+        // courses.add("MATA23");
+        // courses.add("MATA30");
+        // courses.add("MATA31");
+        // courses.add("MATA32");
     }
 
     @Override
-    public boolean meetsGPARequirements(POStGPAInfo marks) {
+    public boolean hasAllCourses(POStGPAInfo info) {
+        // for(int i = 0; i < courses.size(); i++) {
+        //     String course = courses.get(i);
+        //     if(info.getAnswer(course) == (-1)) {
+        //         return false;
+        //     }
+        // }
+
+        return linGroup(info);
+    }
+
+    @Override
+    public boolean meetsGPARequirements(POStGPAInfo info) {
         return false;
     }
 }
