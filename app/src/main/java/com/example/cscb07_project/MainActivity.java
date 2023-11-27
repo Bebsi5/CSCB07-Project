07ef2b695dce2e009d18dde7ff65a30e71755f50
cailene <144947050+cailene@users.noreply.github.com>
2023-11-26 23:49:32 -0500

    Modified MainActivity. Will remember to fix...

--- app/src/main/java/com/example/cscb07_project/MainActivity.java
+++ app/src/main/java/com/example/cscb07_project/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAnchorView(R.id.fab)
                         .setAction("Action", null).show();*/
+                // made changes here!!!! remember that!!
                 Intent intent = new Intent(MainActivity.this, EventList.class);
                 startActivity(intent);
             }
