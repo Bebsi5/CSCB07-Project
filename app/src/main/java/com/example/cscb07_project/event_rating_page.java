c92c7baa3cc892a9446b98b48efc08720d310082
dan-fu2004 <dan.fu@mail.utoronto.ca>
2023-11-28 15:47:02 -0500

    just testing

---
+++ app/src/main/java/com/example/cscb07_project/event_rating_page.java
@@ -0,0 +1,14 @@
+package com.example.cscb07_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class event_rating_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_rating_page);
+    }
+}
