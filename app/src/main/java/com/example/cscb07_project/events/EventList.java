5d71aeaeb4245be52815146572e418712948d77c
cailene <144947050+cailene@users.noreply.github.com>
2023-12-03 17:14:05 -0500

    fixed a bug

--- app/src/main/java/com/example/cscb07_project/events/EventList.java
+++ app/src/main/java/com/example/cscb07_project/events/EventList.java
@@ -147,8 +147,9 @@ public class EventList extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(EventList.this, MainActivity.class);
-                startActivity(intent);
+                /*Intent intent = new Intent(EventList.this, MainActivity.class);
+                startActivity(intent);*/
+                finish();
             }
         });
     }
