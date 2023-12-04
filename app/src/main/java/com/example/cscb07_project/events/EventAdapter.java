b966da43b82cf14613d0678c231bf3b0b670b07d
cailene <144947050+cailene@users.noreply.github.com>
2023-12-04 02:47:53 -0500

    Styling and cleaning up code
    Removing comments, rearranging lines of code, cleaning up and fixing design layout, fixing back button

--- app/src/main/java/com/example/cscb07_project/events/EventAdapter.java
+++ app/src/main/java/com/example/cscb07_project/events/EventAdapter.java
@@ -20,11 +20,6 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
-/**
- * EventAdapter, is an adapter for a RecyclerView
- * It adapts the list of events to the RecyclerView by creating views for individual items
- * and binding the data to those views
- */
 public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
     Context context;
     ArrayList<Event> eventList;
@@ -38,8 +33,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-
-        TextView eventName;
+        TextView eventName, eventDetails;
         Button rsvpButton, deleteEventButton;
         CardView mainCard;
         Button ratingButton;
@@ -47,6 +41,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             eventName = itemView.findViewById(R.id.event_name);
+            eventDetails = itemView.findViewById(R.id.event_details);
             rsvpButton = itemView.findViewById(R.id.rsvp_button);
             deleteEventButton = itemView.findViewById(R.id.delete_event_button);
             mainCard = itemView.findViewById(R.id.main_card);
@@ -63,26 +58,19 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        // Retrieves the Event object at the given position in the eventList
         Event event = eventList.get(position);
-        Log.e("eventadapteronbindview", "event id?: " + event);
 
         holder.eventName.setText(event.getEventName());
+        holder.eventDetails.setText(event.getEventDetails());
 
-        // when the event is clicked, navigates to EventDetails class
         holder.mainCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), EventDetails.class);
                 intent.putExtra("Event ID", event.getEventId());
-
-                Log.d("ClickEvent", "Button clicked for event: " + event.getEventName());
-                Log.d("ClickEvent", "Button clicked for event: " + event.getEventDetails());
-
                 v.getContext().startActivity(intent);
             }
         });
-        // when the rsvp button is clicked, updates student's rsvp status
         holder.rsvpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -101,37 +89,24 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
             }
         });
 
-        // toggling delete button visibility depending on user permissions
-//        if (adminAccess) {
-//            holder.deleteEventButton.setVisibility(View.VISIBLE);
-//            // deletes an event from the database
-//            // but does NOT delete the event from the student data
-//            /*
-//            Ex: Events
-//                - id 1 (deleted)
-//                - id 2
-//
-//                Users
-//                - student 1
-//                    - Events
-//                        - id 1: true (still exists for the student)
-//                        - id 2: true
-//                - student 2
-//             */
-//            // will cause bugs
-//            holder.deleteEventButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    DatabaseReference eventRef = FirebaseDatabase.getInstance().getReference("Events").child(event.getEventId());
-//                    eventRef.removeValue();
-//                }
-//            });
-//        } else {
-//            holder.deleteEventButton.setVisibility(View.GONE);
-//        }
+      // toggling delete button visibility depending on user permissions
+        if (adminAccess) {
+            holder.deleteEventButton.setVisibility(View.VISIBLE);
+        } else {
+            holder.deleteEventButton.setVisibility(View.GONE);
+        }
+
+        // deletes an event from the database
+        // but does NOT delete the event from the student data
+        holder.deleteEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference eventRef = FirebaseDatabase.getInstance().getReference("Events").child(event.getEventId());
+                eventRef.removeValue();
+            }
+        });
     }
 
-    //Returns the total number of items in the RecyclerView
     @Override
     public int getItemCount() {
         return eventList.size();
--- app/src/main/java/com/example/cscb07_project/events/EventDetails.java
+++ app/src/main/java/com/example/cscb07_project/events/EventDetails.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
+import com.example.cscb07_project.Admin;
 import com.example.cscb07_project.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,11 +21,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-
-/**
- * display the details of a specific event, retrieved from the Firebase Realtime Database,
- * and allows the user to update their RSVP status of that event
- */
 public class EventDetails extends AppCompatActivity {
     TextView retrieveEventName, retrieveEventDetails,
             retrieveEventDate, retrieveEventParLim;
@@ -32,30 +28,22 @@ public class EventDetails extends AppCompatActivity {
     String retrieveEventId;
     RSVPFunctionality rsvpFunctionality;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
 
-
-        rsvpFunctionality = new RSVPFunctionality(this);
-
-
-        // Initializes the UI elements
         retrieveEventName = findViewById(R.id.eventDetailName);
         retrieveEventDetails = findViewById(R.id.eventDetails);
         retrieveEventRSVP = findViewById(R.id.rsvpButton);
         retrieveEventDate = findViewById(R.id.eventDate);
         retrieveEventParLim = findViewById(R.id.participantLimit);
 
-
-        // Retrieves the event ID passed from the previous activity (EventList) through the intent
         retrieveEventId = getIntent().getStringExtra("Event ID");
         retrieveEventData(retrieveEventId);
 
+        rsvpFunctionality = new RSVPFunctionality(this);
 
-        // Sets a click listener on the RSVP button to update the RSVP status when clicked
         retrieveEventRSVP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,33 +53,20 @@ public class EventDetails extends AppCompatActivity {
             }
         });
 
-
-
-
-        // back button
         View backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                onBackPressed();
             }
         });
     }
 
 
-    /** Retrieves event data from the Firebase Realtime Database based on the provided event ID
-     *
-     * @param eventId is the event data from the Firebase Realtime Database
-     */
     private void retrieveEventData(String eventId) {
-        // gets reference to "eventId" child key from "Events" key
         DatabaseReference eventRef = FirebaseDatabase.getInstance().getReference("Events").child(eventId);
 
-        // listeners to changes to db and displays the UI based on db
         eventRef.addValueEventListener(new ValueEventListener() {
-            // onDataChange method, checks if the snapshot of the data exists,
-            // then retrieves the Event object and updates the corresponding ids with
-            // the event data
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
@@ -109,5 +84,12 @@ public class EventDetails extends AppCompatActivity {
                 Log.e("EventDetails", "Error reading data from Firebase", error.toException());
             }
         });
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(EventDetails.this, EventList.class);
+        startActivity(intent);
+        finish();
     }
 }
